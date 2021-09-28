4a18c7f9175131a388ee695e0693da79d18af3f1
daniel <dan3455527@gmail.com>
2021-09-28 11:14:55 +0800

    week one commit

--- week1/week1.py
+++ week1/week1.py
@@ -1,9 +1,18 @@
-class Test:
-    def print_someting():
-        input_string = input("input someting here")
-        print(input_string)
-        return None
+# week one exercise, algorithm
+import math
 
+class Exercise:
+    def get_common_divider(int1, int2):
+        int_list = sorted((int1, int2))
+        while True:
+            R = int_list[1] % int_list[0]
+            if R != 0:
+                int_list[1] = int_list[0]
+                int_list[0] = R
+            else:
+                break
+        return int_list[0] # common divider
+            
 
 if __name__ == "__main__":
-    Test.print_someting()
+    print(Exercise.get_common_divider())
