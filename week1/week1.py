99917378e463688d5968e4c44837bb90894dec19
daniel <dan3455527@gmail.com>
2021-09-28 09:35:14 +0800

    initial commit

---
+++ week1/week1.py
@@ -0,0 +1,9 @@
+class Test:
+    def print_someting():
+        input_string = input("input someting here")
+        print(input_string)
+        return None
+
+
+if __name__ == "__main__":
+    Test.print_someting()
