9b8d604fcd06b00f865cf34da0fe2bce502a7e1d
daniel <dan3455527@gmail.com>
2021-11-03 21:12:51 +0800

    delete advanced syntax

--- Hw4_answer/question_4.py
+++ Hw4_answer/question_4.py
@@ -1,12 +1,8 @@
 # question 4
+# bmi calculation
 
-def bmi_calculation():
-    weight = float(input("input the weight(kg): "))
-    height = float(input("input the height(meter): "))
-    bmi = weight/(height ** 2)
-    print(f"your BMI is : {bmi:.2f} kg/m^2")
-    return None
-
-if __name__ == "__main__":
-    bmi_calculation()
+weight = float(input("input the weight(kg): "))
+height = float(input("input the height(meter): "))
+bmi = weight/(height ** 2)
+print("your BMI is :", bmi, "kg/m^2")
 
--- Hw4_answer/question_5.py
+++ Hw4_answer/question_5.py
@@ -1,19 +1,14 @@
 # question 4
+# metric conversion
 
 
-def metric_conversion():
-    distance = float(input("enter the distance travelled (in km): "))
-    volume = float(input("volume of tank (in liter): "))
+distance = float(input("enter the distance travelled (in km): "))
+volume = float(input("volume of tank (in liter): "))
 
-    fuel_mileage_rate = volume / (100 * distance)
-    fuel_efficiency = distance / volume
-    mpg = (distance * 0.621) / (volume * 0.264) # 1 km = 0.621 mile, 1 liter = 0.264 gallon
+fuel_mileage_rate = volume / (100 * distance)
+fuel_efficiency = distance / volume
+mpg = (distance * 0.621) / (volume * 0.264) # 1 km = 0.621 mile, 1 liter = 0.264 gallon
 
-    print(f"result:\nfuel mileage rate = {fuel_mileage_rate:.3f}\n"\
-          f"fuel efficiecy    = {fuel_efficiency:.3f}\n"\
-          f"mpg               = {mpg:.3f}")
-    return None
-
-
-if __name__ == "__main__":
-    metric_conversion()
+print("fuel mileage rate =  ", fuel_mileage_rate)
+print("fuel efficiecy    =  ", fuel_efficiency)
+print("mpg               =  ", mpg)
